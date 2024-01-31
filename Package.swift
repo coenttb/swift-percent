780ea5e0e6cfeb2e89d8d45529ffe06d6db8f5b3
Coen ten Thije Boonkkamp <coen@puzl.legal>
2024-01-31 16:52:31 +0100

    swift-percent

--- Package.swift
+++ Package.swift
@@ -1,15 +1,23 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
+extension String {
+    static let percent:Self = "Percent"
+}
+
+extension Target.Dependency {
+    static let percent:Self = .target(name: .percent)
+}
 
 import PackageDescription
 
 let package = Package(
-    name: "Percent",
+    name: "swift-percent",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Percent",
-            targets: ["Percent"]),
+            name: .percent,
+            targets: [.percent]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +27,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Percent",
-            dependencies: []),
-        .testTarget(
-            name: "PercentTests",
-            dependencies: ["Percent"]),
+            name: .percent,
+            dependencies: []
+        )
     ]
 )
